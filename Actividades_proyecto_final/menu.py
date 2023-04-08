08c1cc81f7471ca61e3fc01ba19c96df3c716101
Lara <70819213+larahoffman@users.noreply.github.com>
2023-04-08 15:19:10 -0300

    Segunda_preentrega_Hoffman

---
+++ Actividades_proyecto_final/menu.py
@@ -0,0 +1,9 @@
+# import
+from paquete.modulo1 import Cliente
+from paquete.modulo2 import menu
+# objetos
+cliente1 = Cliente(1, "Gabriel Wanchupen", 23, "gabyla@gmail.com", "11-54338976")
+
+print(cliente1)
+
+menu()
