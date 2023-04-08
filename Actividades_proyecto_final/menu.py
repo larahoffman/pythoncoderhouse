e5209371e74ef75786b430d279d9b2a194107dd8
Lara <70819213+larahoffman@users.noreply.github.com>
2023-04-08 15:19:43 -0300

    Delete menu.py

--- Actividades_proyecto_final/menu.py
+++
@@ -1,9 +0,0 @@
-# import
-from paquete.modulo1 import Cliente
-from paquete.modulo2 import menu
-# objetos
-cliente1 = Cliente(1, "Gabriel Wanchupen", 23, "gabyla@gmail.com", "11-54338976")
-
-print(cliente1)
-
-menu()
