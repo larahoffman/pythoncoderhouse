60d62ed940b0e79c5fcd409092fcade006cbff54
larahoffman <larahoff02@gmail.com>
2023-04-10 21:25:04 -0300

    se comento la linea 4 - no se usa el menu

--- Actividades_proyecto_final/Segunda_preentrega_Hoffman/menu.py
+++ Actividades_proyecto_final/Segunda_preentrega_Hoffman/menu.py
@@ -1,12 +1,13 @@
 from base_cliente.cliente import *
 from cliente_mayorista.mayorista import ClienteMayorista
 from cliente_minorista.minorista import ClienteMinorista
-from primera_preentrega.funciones_menu import menu
+# from primera_preentrega.funciones_menu import menu 
 
 cliente1 = ClienteMayorista(1, "Murisco Company", "contacto@murisco.com.ar", "11-33765091", 1000)
 cliente2 = ClienteMinorista(2, "Juan Perez", "juano@gmail.com", "11-54338976", 5)
 
-print("Datos")
+print("Datos:")
+print("")
 print(cliente1)
 print("")
 print(cliente2)
@@ -15,4 +16,4 @@ print("Acciones:")
 cliente1.enviar_correo()
 cliente2.cambiar_telefono("11-44536166")
 
-print(f"El nuevo número de télefono es:{cliente2.telefono}")
+print(f"El nuevo número de télefono del cliente {cliente2.nombre} es: {cliente2.telefono}")
