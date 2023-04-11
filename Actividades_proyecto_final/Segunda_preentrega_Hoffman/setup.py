42e7f10b9446faee95160209d6e1523aeb699b70
larahoffman <larahoff02@gmail.com>
2023-04-10 21:25:54 -0300

    para el paquete redistribuible

---
+++ Actividades_proyecto_final/Segunda_preentrega_Hoffman/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='Segunda_preentrega_Hoffman',
+    version='0.1.0',
+    author='Lara Hoffman',
+    author_email='larahoff02@gmail.com',
+    description='Esta es la segunda pre-entrega del proyecto final de Python',
+    packages=['base_cliente', 'cliente_mayorista', 'cliente_minorista','primera_preentrega']
+)
